f0473e8b56e97f440353ffd1837cb48420185632
Gabriel Garcia <devgglop@gmail.com>
2021-06-05 13:19:21 +0200

    Moved completeChore to editChore as it is the same operation

--- functions/index.js
+++ functions/index.js
@@ -75,6 +75,7 @@ exports.editChore = functions.firestore
 
       console.log("Updated chore with id: " + choreId);
 
+      // TODO: Mejorar el código un poco
       const documentRef = database.collection("groups")
           .doc(groupId).collection("users").doc(newChore.assignee);
 
@@ -89,52 +90,74 @@ exports.editChore = functions.firestore
 
               console.log("Obtained user from chore: " + userId);
 
-              message.token = user.messagingToken;
-              message.notification.title = "Updated chore!";
+              if (newChore.isCompleted != oldChore.isCompleted &&
+                  newChore.isCompleted && !oldChore.isCompleted) {
+                const creatorRef = database.collection("groups")
+                    .doc(groupId).collection("users").doc(newChore.creator);
 
-              let updatedFieldsMsg = "The ";
-              const changedFields = [];
-              if (newChore.assignee != oldChore.assignee) {
-                changedFields.push("Assignee");
-              }
-              const newExpirationDate =
-                  newChore.expiration.toDate().toDateString();
-              const oldExpirationDate =
-                  oldChore.expiration.toDate().toDateString();
-              if (newExpirationDate != oldExpirationDate) {
-                changedFields.push("Expiration date");
-              }
-              if (newChore.name != oldChore.name) {
-                changedFields.push("Name");
-              }
-              if (newChore.points != oldChore.points) {
-                changedFields.push("Importance");
-              }
-
-              for (let x = 0; x < changedFields.length; x++) {
-                if (x == changedFields.length - 2) {
-                  updatedFieldsMsg = updatedFieldsMsg.concat(
-                      changedFields[x] + " and ");
-                } else if (x != changedFields.length - 1) {
-                  updatedFieldsMsg = updatedFieldsMsg.concat(
-                      changedFields[x] + ", ");
-                } else {
-                  updatedFieldsMsg = updatedFieldsMsg.concat(
-                      changedFields[x]);
-                }
-              }
-
-              if (changedFields.length >= 2) {
-                updatedFieldsMsg = updatedFieldsMsg.concat(" were updated.");
-              } else if (changedFields.length == 1) {
-                updatedFieldsMsg = updatedFieldsMsg.concat(" was updated.");
+                creatorRef.get()
+                    .then((creatorSnapshot) => {
+                      if (creatorSnapshot.exists) {
+                        const creator = creatorSnapshot.data();
+                        message.token = creator.messagingToken;
+                        message.notification.title = "Completed chore!";
+                        message.notification.body =
+                            "The chore '" + newChore.name + "' was completed";
+                      } else {
+                        console.log("The snapshot did not exist");
+                      }
+                    })
+                    .catch((error) => {
+                      console.log("Error getting the user to notify: " + error);
+                    });
               } else {
-                updatedFieldsMsg = "";
-              }
+                message.token = user.messagingToken;
+                message.notification.title = "Updated chore!";
 
-              message.notification.body =
-                  "The chore '" + oldChore.name + "' was modified. " +
-                  updatedFieldsMsg;
+                let updatedFieldsMsg = "The ";
+                const changedFields = [];
+                if (newChore.assignee != oldChore.assignee) {
+                  changedFields.push("Assignee");
+                }
+                const newExpirationDate =
+                    newChore.expiration.toDate().toDateString();
+                const oldExpirationDate =
+                    oldChore.expiration.toDate().toDateString();
+                if (newExpirationDate != oldExpirationDate) {
+                  changedFields.push("Expiration date");
+                }
+                if (newChore.name != oldChore.name) {
+                  changedFields.push("Name");
+                }
+                if (newChore.points != oldChore.points) {
+                  changedFields.push("Importance");
+                }
+
+                for (let x = 0; x < changedFields.length; x++) {
+                  if (x == changedFields.length - 2) {
+                    updatedFieldsMsg = updatedFieldsMsg.concat(
+                        changedFields[x] + " and ");
+                  } else if (x != changedFields.length - 1) {
+                    updatedFieldsMsg = updatedFieldsMsg.concat(
+                        changedFields[x] + ", ");
+                  } else {
+                    updatedFieldsMsg = updatedFieldsMsg.concat(
+                        changedFields[x]);
+                  }
+                }
+
+                if (changedFields.length >= 2) {
+                  updatedFieldsMsg = updatedFieldsMsg.concat(" were updated.");
+                } else if (changedFields.length == 1) {
+                  updatedFieldsMsg = updatedFieldsMsg.concat(" was updated.");
+                } else {
+                  updatedFieldsMsg = "";
+                }
+
+                message.notification.body =
+                    "The chore '" + oldChore.name + "' was modified. " +
+                    updatedFieldsMsg;
+              }
 
               console.log("Sending message: " + JSON.stringify(message));
 
@@ -191,56 +214,6 @@ exports.deleteChore = functions.firestore
                   .catch((error) => {
                     console.log("Error sending notification: " + error);
                   });
-            } else {
-              console.log("The snapshot did not exist");
-            }
-          })
-          .catch((error) => {
-            console.log("Error getting the user to notify: " + error);
-          });
-    });
-
-exports.completeChore = functions.firestore
-    .document("groups/{groupId}/chores/{choreId}")
-    .onUpdate((change, context) => {
-      const groupId = context.params.groupId;
-      const choreId = context.params.choreId;
-      const newChore = change.after.data();
-      const oldChore = change.before.data();
-
-      console.log("Updated chore with id: " + choreId);
-
-      const documentRef = database.collection("groups")
-          .doc(groupId).collection("users").doc(newChore.creator);
-
-      console.log("Got document reference pointing to user at " +
-            documentRef.path);
-
-      return documentRef.get()
-          .then((documentSnapshot) => {
-            if (documentSnapshot.exists) {
-              if (newChore.isCompleted != oldChore.isCompleted &&
-                  newChore.isCompleted && !oldChore.isCompleted) {
-                const user = documentSnapshot.data();
-                const userId = documentSnapshot.id;
-
-                console.log("Obtained user from chore: " + userId);
-
-                message.token = user.messagingToken;
-                message.notification.title = "Completed chore!";
-                message.notification.body =
-                    "The chore '" + newChore.name + "' was completed";
-
-                console.log("Sending message: " + JSON.stringify(message));
-
-                admin.messaging().send(message)
-                    .then((response) => {
-                      console.log("Successfully sent message: " + response);
-                    })
-                    .catch((error) => {
-                      console.log("Error sending notification: " + error);
-                    });
-              }
             } else {
               console.log("The snapshot did not exist");
             }
